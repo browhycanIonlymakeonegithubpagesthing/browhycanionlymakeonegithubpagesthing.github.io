02fd8b1a6654bff9321ebfbb34bb205d2aacd044
insert-text-here <125519936+insert-text-here@users.noreply.github.com>
2023-03-16 22:42:39 -0700

    Update gradient.js

--- files/gradient.js
+++ files/gradient.js
@@ -1,12 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-	const headerText = document.getElementById("header-text");
+document.addEventListener("DOMContentLoaded", function () {
+  const headerText = document.getElementById("header-text");
+  const hiddenBtn = document.getElementById("hidden-btn");
 
-	document.body.addEventListener("animationiteration", function() {
-		const currentBg = getComputedStyle(document.body).backgroundImage;
-		if (currentBg.includes("black")) {
-			headerText.style.color = "white";
-		} else {
-			headerText.style.color = "black";
-		}
-	});
+  document.body.addEventListener("animationiteration", function () {
+    const currentBg = getComputedStyle(document.body).backgroundImage;
+    if (currentBg.includes("black")) {
+      headerText.style.color = "white";
+    } else {
+      headerText.style.color = "black";
+    }
+  });
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "`") {
+      hiddenBtn.style.display = hiddenBtn.style.display === "none" ? "block" : "none";
+    }
+  });
 });
