3d2b1431ac0321b5e135f9a2ebebb51351b11d68
insert-text-here <125519936+insert-text-here@users.noreply.github.com>
2023-03-28 21:37:57 -0700

    Update gradient.js

--- files/gradient.js
+++ files/gradient.js
@@ -1,23 +1,39 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hiddenBtn = document.getElementById("hidden-btn");
-
-  document.addEventListener("keydown", function (event) {
-    if (event.keyCode === 192) { // keyCode 192 corresponds to the "`" key
-      hiddenBtn.style.display = hiddenBtn.style.display === "none" ? "block" : "none";
-    }
-  });
-});
-
 const textList = [
-	"Insert Text Here",
-	"Javascript Is Cool",
-	"Go Touch Grass",
-	"outdoorschool",
-	"Crazygird=Gamign"
+  "Insert Text Here",
+  "Javascript Is Cool",
+  "Go Touch Grass",
+  "outdoorschool",
+  "Crazygird=Gamign"
 ];
 
+const dadJokes = [
+  "what did the mathematician say to his insurance company when he got into a car accident?",
+  "his car got totalled!",
+  "hahaha....."
+];
+
+const audioList = [
+  "https://insert-text-here.github.io/files/doublekill.mp3",
+  "https://insert-text-here.github.io/files/gettrolled.mp3"
+];
+
+let dadJokeIndex = 0;
+
 function changeText() {
-	const clickableText = document.getElementById("clickable-text");
-	const randomIndex = Math.floor(Math.random() * textList.length);
-	clickableText.textContent = textList[randomIndex];
+  const random = Math.random();
+  if (random < 0.05 && dadJokeIndex < dadJokes.length) {
+    document.getElementById("clickable-text").innerHTML = dadJokes[dadJokeIndex];
+    dadJokeIndex++;
+  } else {
+    const index = Math.floor(Math.random() * textList.length);
+    document.getElementById("clickable-text").innerHTML = textList[index];
+  }
+
+  if (random < 0.01) {
+    const audioIndex = Math.floor(Math.random() * audioList.length);
+    const audio = new Audio(audioList[audioIndex]);
+    audio.play();
+  }
 }
+
+document.getElementById("clickable-text").addEventListener("click", changeText);
