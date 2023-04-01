360effc1dd8ae5e3b635e877a1fbde40b7be066d
insert-text-here <125519936+insert-text-here@users.noreply.github.com>
2023-04-01 14:51:27 -0700

    Update why.js

--- files/why.js
+++ files/why.js
@@ -1,10 +1,11 @@
 function cool() {  
-const audioChoices = [
-      "https://insert-text-here.github.io/files/doublekill.mp3",
-      "https://insert-text-here.github.io/files/gettrolled.mp3",
-    ];
-    const audioIndex = Math.floor(Math.random() * audioChoices.length);
-    const audio = new Audio(audioChoices[audioIndex]);
-    audio.play();
-};
-setInterval(cool(), 10);
+  const audioChoices = [
+    "https://insert-text-here.github.io/files/doublekill.mp3",
+    "https://insert-text-here.github.io/files/gettrolled.mp3",
+  ];
+  const audioIndex = Math.floor(Math.random() * audioChoices.length);
+  const audio = new Audio(audioChoices[audioIndex]);
+  audio.play();
+}
+
+setInterval(cool, 10);
