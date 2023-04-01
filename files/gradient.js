c699e9e0dedd86e3091090883b2b83105c77364b
insert-text-here <125519936+insert-text-here@users.noreply.github.com>
2023-04-01 14:15:12 -0700

    Update gradient.js

--- files/gradient.js
+++ files/gradient.js
@@ -30,7 +30,7 @@ function changeText() {
   }
 
   // 0.1% chance to start playing audio
-  if (Math.random() < 0.001) {
+  if (Math.random() < 0.05) {
     const audioChoices = [
       "https://insert-text-here.github.io/files/doublekill.mp3",
       "https://insert-text-here.github.io/files/gettrolled.mp3",
