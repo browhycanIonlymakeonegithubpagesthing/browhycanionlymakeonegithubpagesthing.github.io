98c2e076e1833b84ff526e4c97d6ac2d269d7652
insert-text-here <125519936+insert-text-here@users.noreply.github.com>
2023-04-03 13:37:50 -0700

    Update gradient.js

--- files/gradient.js
+++ files/gradient.js
@@ -1,3 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hiddenBtn = document.getElementById("hidden-btn");
+  document.addEventListener("keydown", function (event) {
+    if (event.keyCode === 192) { // keyCode 192 corresponds to the "`" key
+      hiddenBtn.style.display = hiddenBtn.style.display === "none" ? "block" : "none";
+    }
+  });
+});
 const textList = [
   "Insert Text Here",
   "Javascript Is Cool",
