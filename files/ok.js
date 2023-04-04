a53dd2176ec658f4a823a760894aa5e17348caa1
insert-text-here <125519936+insert-text-here@users.noreply.github.com>
2023-04-03 18:51:03 -0700

    Update ok.js

--- files/ok.js
+++ files/ok.js
@@ -1,46 +1,51 @@
 function openTab(evt, tabName) {
   var i, tabcontent, tablinks;
+
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
+
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(tabName).style.display = "block";
+
+  var tab = document.getElementById(tabName);
+  tab.style.display = "block";
   evt.currentTarget.className += " active";
-  var iframe = document.getElementById(tabName).querySelector("iframe");
+
+  var iframe = tab.querySelector("iframe");
   if (!iframe.hasAttribute("src")) {
     iframe.src = "https://agent-gamer-cool-thing.474652.workers.dev";
   }
 }
 
 function newTab() {
-  var numTabs = document.getElementsByClassName("tablinks").length;
-  if (numTabs < 12) {
-    numTabs++;
-    var tabButton = document.createElement("button");
-    tabButton.className = "tablinks";
-    tabButton.innerHTML = "Tab " + numTabs;
-    tabButton.onclick = function () {
-      openTab(event, "Tab" + numTabs);
-    };
-    var tabContent = document.createElement("div");
-    tabContent.id = "Tab" + numTabs;
-    tabContent.className = "tabcontent";
-    tabContent.innerHTML = "<iframe></iframe>";
-    var tabDiv = document.getElementsByClassName("tab-left")[0];
-    tabDiv.insertBefore(tabButton, tabDiv.lastElementChild);
-    document.body.appendChild(tabContent);
-  }
+  var numTabs = document.getElementsByClassName("tablinks").length + 1;
+  if (numTabs > 12) return;
+  
+  var tabButton = document.createElement("button");
+  tabButton.className = "tablinks";
+  tabButton.innerHTML = "Tab " + numTabs;
+  tabButton.onclick = function () {
+    openTab(event, "Tab" + numTabs);
+  };
+  var tabContent = document.createElement("div");
+  tabContent.id = "Tab" + numTabs;
+  tabContent.className = "tabcontent";
+  tabContent.innerHTML = "<iframe></iframe>";
+
+  var tabDiv = document.getElementsByClassName("tab-left")[0];
+  tabDiv.insertBefore(tabButton, tabDiv.lastElementChild);
+  document.body.appendChild(tabContent);
 }
 
 function openSettings() {
-  var menu = document.getElementById("settings-menu");
-  if (menu.style.display === "none") {
-    menu.style.display = "block";
+  var settingsMenu = document.getElementById("settings-menu");
+  if (settingsMenu.style.display === "none") {
+    settingsMenu.style.display = "block";
   } else {
-    menu.style.display = "none";
+    settingsMenu.style.display = "none";
   }
 }
